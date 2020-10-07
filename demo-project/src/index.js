5b76840c43c3540b793f6d3844b0c64a4843cee9
Mehul Parmar <mehulparmar10091999@gamil.com>
2020-10-07 09:42:06 +0530

    interaction between React Components

--- demo-project/src/index.js
+++ demo-project/src/index.js
@@ -9,15 +9,16 @@ import './index.css';
 
 
 // ------------- with out jsx -------------------
-// const eelement = React.createElement("h1",null,"welcome to React Programming word ")
+// const eelement = React.createElement("h1",{className:'testClass'},"welcome to React Programming word ")
 // ReactDOM.render(eelement,document.getElementById('root'));
 
 
 //---------------- To display text ----------------
-// const element1 = React.createElement("div",{className:"testClass"},
-// React.createElement("h1",null,"welcome to React Programming"),
-// React.createElement("h1",null,"we are ready to go with React...."));
-// ReactDOM.render(element1,document.getElementById('root'));
+// const element1 = React.createElement("div", { className: "testClass" },
+//     React.createElement("h1", null, "welcome to React Programming"),
+//     React.createElement("h1", null, "we are ready to go with React...."));
+// ReactDOM.render(element1, document.getElementById('root'));
+
 
 //---------------- User Define component display using function --------------
 // function DisplayEmployeeInfo(employee) {
@@ -36,6 +37,7 @@ import './index.css';
 
 // const element = <DisplayEmployeeInfo Id="101" Name="Mahendra" Location="Botad"></DisplayEmployeeInfo>
 // ReactDOM.render(element,document.getElementById('root'));
+
 
 //---------------- User Define component display using error function --------------
 // var DisplayEmployeeInfo=(employee)=> {
@@ -98,21 +100,94 @@ import './index.css';
 
 
 //------------------------------ introduction to State -------------------
-class Employee extends React.Component{
-  state={counter:0};
-  addemploy=()=>{
-    this.setState({counter:this.state.counter+1});
-  }
-  render(){
-    return<div>
-      <h1>welcome to employee Class ......</h1>
-      <p>
-        <button onClick={this.addemploy}>Add an Employee</button>
-      </p>
-      <p>Add Employee Button Clicked:<b>{this.state.counter} time</b></p>
-    </div>
-  }
+// class Employee extends React.Component{
+//   state={counter:0};
+//   addemploy=()=>{
+//     this.setState({counter:this.state.counter+1});
+//   }
+//   render(){
+//     return<div>
+//       <h1>welcome to employee Class ......</h1>
+//       <p>
+//         <button onClick={this.addemploy}>Add an Employee</button>
+//       </p>
+//       <p>Add Employee Button Clicked:<b>{this.state.counter} time</b></p>
+//     </div>
+//   }
+// }
+
+// const element = <Employee></Employee>
+// ReactDOM.render(element,document.getElementById('root'));
+
+
+//---------------------------- Intraction Between React Components -----------------------
+class Employee extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state={
+            updatedSalary: null
+        };
+    }
+
+    getUpdatedSalary = (salary) => {
+        this.setState({ updatedSalary: salary });
+    }
+
+    render() {
+        return <div>
+            <h1>Employee Component.....</h1>
+            <p>
+                <label>Employee Id:<b>{this.props.Id}</b></label>
+            </p>
+            <p>
+                <label>Employee Name:<b>{this.props.Name}</b></label>
+            </p>
+            <p>
+                <label>Employee Location:<b>{this.props.Location}</b></label>
+            </p>
+            <p>
+                <label>Total Salary:<b>{this.props.Salary}</b></label>
+            </p>
+            <p>
+                <label>Updated Salary :<b>{this.state.updatedSalary}</b></label>
+            </p>
+            <Salary BasicSalary={this.props.BasicSalary} HRA={this.props.HRA} SpecialAllowance={this.props.SpecialAllowance} onSalaryChanged={this.getUpdatedSalary}></Salary>
+        </div>
+    }
 }
 
-const element = <Employee></Employee>
-ReactDOM.render(element,document.getElementById('root'));
+class Salary extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            basic: this.props.BasicSalary,
+            hra: this.props.HRA,
+            sa: this.props.SpecialAllowance
+        };
+    }
+
+    updateSalary = () => {
+        let salary = parseInt(this.refs.basic.value) + parseInt(this.refs.hra.value) +
+            parseInt(this.refs.sa.value);
+        this.props.onSalaryChanged(salary);
+    }
+
+    render() {
+        return <div>
+            <h1>Salary Component.....</h1>
+            <p>
+                <label>Basic Salaty :<input type="text" ref="basic" defaultValue={this.state.basic}></input></label>
+            </p>
+            <p>
+                <label>HRA :<input type="text" ref="hra" defaultValue={this.state.hra}></input></label>
+            </p>
+            <p>
+                <label>Specila Allowance :<input type="text" ref="sa" defaultValue={this.state.sa}></input></label>
+            </p>
+            <button onClick={this.updateSalary}>Update</button>
+        </div>
+    }
+}
+
+const element = <Employee Id="101" Name="Mahendra" Location="Botad" Salary="25000" BasicSalary="10000" HRA="10000" SpecialAllowance="5000"></Employee>
+ReactDOM.render(element, document.getElementById('root'));
