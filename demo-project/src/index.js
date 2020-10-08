4b778bc3108c89c6bbf22c726164979599645d9c
Mehul Parmar <mehulparmar10091999@gamil.com>
2020-10-08 12:18:24 +0530

    form velidation using formik

--- demo-project/src/index.js
+++ demo-project/src/index.js
@@ -1,7 +1,6 @@
-import { useFormik } from 'formik';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { useFormik } from 'formik';
 
 // -------------using jsx--------------------
 // const element = <h1>welcome to React Programming word ......</h1>
@@ -248,15 +247,43 @@ import './index.css';
 
 
 //--------------------------- form velidation using formik -------------------------------
+
+const validateEmployee = empData => {
+    const errors = {};
+
+    if (!empData.Name) {
+        errors.Name = "Please Enter Employee Name";
+    } else if (empData.Name.length > 20) {
+        errors.Name = "Employee Name should not execeed 20 chaeacters";
+    }
+
+    if (!empData.Location) {
+        errors.Location = "Please Enter Employee Location";
+    }
+
+    if (!empData.EmailId) {
+        errors.EmailId = "Please Enter Employee EmailId";
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(empData.EmailId)) {
+        errors.EmailId = "Invelid email address";
+    }
+
+    return errors;
+}
+
+
 const EmployeeComponent = () => {
     const formik = useFormik({
         initialValues: {
             Id: '',
             Name: '',
             Location: '',
-            Salary: ''
+            Salary: '',
+            EmailId: ''
         },
-        onsubmit: values => {
+
+        validate: validateEmployee,
+
+        onSubmit: values => {
             alert(JSON.stringify(values));
         }
     });
@@ -264,26 +291,34 @@ const EmployeeComponent = () => {
     return (
         <div>
             <h2>New Employee Form...</h2>
-            <form onsubmit={formik.handleSubmit}>
+            <form onSubmit={formik.handleSubmit}>
                 <p>
                     <label htmlFor="Id">Employee ID : </label>
                     <input type="text" name="Id" id="Id" value={formik.values.Id}
                         onChange={formik.handleChange}></input>
                 </p>
                 <p>
-                    <label htmlFor="Id">Employee Name : </label>
+                    <label htmlFor="Name">Employee Name : </label>
                     <input type="text" name="Name" id="Name" value={formik.values.Name}
-                        onChange={formik.handleChange}></input>
+                        onChange={formik.handleChange} onBlur={formik.handleBlur}></input>
+                    {formik.touched.Name && formik.errors.Name ? <span style={{ color: 'red' }}>{formik.errors.Name}</span> : null}
                 </p>
                 <p>
-                    <label htmlFor="Id">Employee Location : </label>
+                    <label htmlFor="Location">Employee Location : </label>
                     <input type="text" name="Location" id="Location" value={formik.values.Location}
-                        onChange={formik.handleChange}></input>
+                        onChange={formik.handleChange} onBlur={formik.handleBlur}></input>
+                    {formik.touched.Location && formik.errors.Location ? <span style={{ color: 'red' }}>{formik.errors.Location}</span> : null}
                 </p>
                 <p>
-                    <label htmlFor="Id">Employee Salary : </label>
+                    <label htmlFor="Salary">Employee Salary : </label>
                     <input type="text" name="Salary" id="Salary" value={formik.values.Salary}
                         onChange={formik.handleChange}></input>
+                </p>
+                <p>
+                    <label htmlFor="EmailId">Employee EmailId : </label>
+                    <input type="text" name="EmailId" id="EmailId" value={formik.values.EmailId}
+                        onChange={formik.handleChange} onBlur={formik.handleBlur}></input>
+                    {formik.touched.EmailId && formik.errors.EmailId ? <span style={{ color: 'red' }}>{formik.errors.EmailId}</span> : null}
                 </p>
                 <button type="submit">Create</button>
             </form>
