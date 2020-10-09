d36ae68af4b1f6ec3510cde92afa1da53f3eddfb
Mehul Parmar <mehulparmar10091999@gamil.com>
2020-10-09 21:22:31 +0530

    basic design of signup form is done

--- demo-project/src/index.js
+++ demo-project/src/index.js
@@ -7,7 +7,7 @@ import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
-import Badge from 'react-bootstrap/Badge';
+import Card from 'react-bootstrap/Card';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const EmployeeComponent = () => {
@@ -15,17 +15,11 @@ const EmployeeComponent = () => {
         initialValues: {
             Confirm_Password: '',
             Password: '',
-            Id: '',
-            Name: '',
-            Location: '',
-            Salary: '',
             EmailId: ''
         },
 
         validationSchema: yup.object({
-            Name: yup.string().max(20, "Name should not exceed 20 characters").required("please enter the Employee Name"),
             EmailId: yup.string().email("Invelid email address").required("Please Enter Email Id"),
-            Location: yup.string().required("Please Enter A Location"),
             Confirm_Password: yup.string().required('Confirm Password is required').oneOf([yup.ref('Password'), null], 'Passwords does not match'),
             Password: yup.string().required("Please Enter A Password"),
         }),
@@ -39,9 +33,9 @@ const EmployeeComponent = () => {
         <div>
             <Container className="mt-4 pl-4 pr-4 pt-4 pb-4 col-lg-6 shadow p-3 mb-5 bg-white rounded">
                 <Row>
-                    <Col xs lg="8">
+                    <Col xs lg="12">
                         <h1>
-                            <Badge variant="secondary">Register Here</Badge>
+                            <Card.Header>Register Here</Card.Header>
                         </h1>
                         <Form onSubmit={formik.handleSubmit}>
                             <Form.Group controlId="formBasicEmail">
@@ -66,39 +60,6 @@ const EmployeeComponent = () => {
                     </Col>
                 </Row>
             </Container>
-            <h2>New Employee Form...</h2>
-            <form onSubmit={formik.handleSubmit}>
-                <p>
-                    <label htmlFor="Id">Employee ID : </label>
-                    <input type="text" name="Id" id="Id" value={formik.values.Id}
-                        onChange={formik.handleChange}></input>
-                </p>
-                <p>
-                    <label htmlFor="Name">Employee Name : </label>
-                    <input type="text" name="Name" id="Name" value={formik.values.Name}
-                        onChange={formik.handleChange} onBlur={formik.handleBlur}></input>
-                    {formik.touched.Name && formik.errors.Name ? <span style={{ color: 'red' }}>{formik.errors.Name}</span> : null}
-                </p>
-                <p>
-                    <label htmlFor="Location">Employee Location : </label>
-                    <input type="text" name="Location" id="Location" value={formik.values.Location}
-                        onChange={formik.handleChange} onBlur={formik.handleBlur}></input>
-                    {formik.touched.Location && formik.errors.Location ? <span style={{ color: 'red' }}>{formik.errors.Location}</span> : null}
-                </p>
-                <p>
-                    <label htmlFor="Salary">Employee Salary : </label>
-                    <input type="text" name="Salary" id="Salary" value={formik.values.Salary}
-                        onChange={formik.handleChange}></input>
-                </p>
-                <p>
-                    <label htmlFor="EmailId">Employee EmailId : </label>
-                    <input type="text" name="EmailId" id="EmailId" value={formik.values.EmailId}
-                        onChange={formik.handleChange} onBlur={formik.handleBlur}></input>
-                    {formik.touched.EmailId && formik.errors.EmailId ? <span style={{ color: 'red' }}>{formik.errors.EmailId}</span> : null}
-                </p>
-                <button type="submit">Create</button>
-                {/* <Button variant="primary">Primary</Button>{' '} */}
-            </form>
         </div>
     )
 }
