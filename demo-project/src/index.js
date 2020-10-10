d02253c9676ac06414c7e5a97df9de623fd5a3bd
Mehul Parmar <mehulparmar10091999@gamil.com>
2020-10-10 11:14:35 +0530

    registration page updated

--- demo-project/src/index.js
+++ demo-project/src/index.js
@@ -7,8 +7,10 @@ import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
-import Card from 'react-bootstrap/Card';
+// import Card from 'react-bootstrap/Card';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import logo from './logo.png';
 
 const EmployeeComponent = () => {
     const formik = useFormik({
@@ -24,7 +26,7 @@ const EmployeeComponent = () => {
             Password: yup.string().required("Please Enter A Password").matches(
                 /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{7,}$/,
                 "Must Contain 7 Characters, One Uppercase, One Lowercase, One Number and one special case Character"
-              )
+            )
         }),
 
         onSubmit: values => {
@@ -33,37 +35,38 @@ const EmployeeComponent = () => {
     });
 
     return (
-        <div>
-            <Container className="mt-4 pl-4 pr-4 pt-4 pb-4 col-lg-6 shadow p-3 mb-5 bg-white rounded">
+        <div className="text-center mb-2">
+            <Container className="mt-4 pl-4 pr-4 pt-4 pb-4 col-lg-4 shadow p-3 mb-5 bg-white rounded">
                 <Row>
                     <Col xs lg="12">
-                        <h1>
-                            <Card.Header>Register Here</Card.Header>
-                        </h1>
                         <Form onSubmit={formik.handleSubmit}>
-                            <Form.Group controlId="formBasicEmail">
-                                <Form.Label>Email address</Form.Label>
-                                <Form.Control type="email" placeholder="Enter email" name="EmailId" id="EmailId" value={formik.values.EmailId} onChange={formik.handleChange} onBlur={formik.handleBlur} />
-                                {formik.touched.EmailId && formik.errors.EmailId ? <span style={{ color: 'red' }}>{formik.errors.EmailId}</span> : null}
-                            </Form.Group>
-                            <Form.Group controlId="formBasicPassword">
-                                <Form.Label>Password</Form.Label>
-                                <Form.Control type="password" placeholder="Password" name="Password" id="Password" value={formik.values.Password} onChange={formik.handleChange} onBlur={formik.handleBlur} />
-                                {formik.touched.Password && formik.errors.Password ? <span style={{ color: 'red' }}>{formik.errors.Password}</span> : null}
-                            </Form.Group>
-                            <Form.Group controlId="formBasicPassword">
-                                <Form.Label>Confirm Password</Form.Label>
-                                <Form.Control type="password" placeholder="Password" name="Confirm_Password" id="Confirm_Password" value={formik.values.Confirm_Password} onChange={formik.handleChange} onBlur={formik.handleBlur} />
-                                {formik.touched.Confirm_Password && formik.errors.Confirm_Password ? <span style={{ color: 'red' }}>{formik.errors.Confirm_Password}</span> : null}
-                            </Form.Group>
-                            <Button variant="primary" type="submit">
-                                Submit
+                            <div >
+                                <img src={logo} alt="Logo" />
+                                <h1 className="h3 mb-3">Register Here</h1>
+                                <Form.Group>
+                                    <Form.Label>Email address</Form.Label>
+                                    <Form.Control type="email" placeholder="Enter email" name="EmailId" id="EmailId" value={formik.values.EmailId} onChange={formik.handleChange} onBlur={formik.handleBlur} />
+                                    {formik.touched.EmailId && formik.errors.EmailId ? <span style={{ color: 'red' }}>{formik.errors.EmailId}</span> : null}
+                                </Form.Group>
+                                <Form.Group>
+                                    <Form.Label>Password</Form.Label>
+                                    <Form.Control type="password" placeholder="Password" name="Password" id="Password" value={formik.values.Password} onChange={formik.handleChange} onBlur={formik.handleBlur} />
+                                    {formik.touched.Password && formik.errors.Password ? <span style={{ color: 'red' }}>{formik.errors.Password}</span> : null}
+                                </Form.Group>
+                                <Form.Group>
+                                    <Form.Label>Confirm Password</Form.Label>
+                                    <Form.Control type="password" placeholder="Password" name="Confirm_Password" id="Confirm_Password" value={formik.values.Confirm_Password} onChange={formik.handleChange} onBlur={formik.handleBlur} />
+                                    {formik.touched.Confirm_Password && formik.errors.Confirm_Password ? <span style={{ color: 'red' }}>{formik.errors.Confirm_Password}</span> : null}
+                                </Form.Group>
+                                <Button variant="primary" type="submit">
+                                    Register
                             </Button>
+                            </div>
                         </Form>
                     </Col>
                 </Row>
             </Container>
-        </div>
+        </div >
     )
 }
 
